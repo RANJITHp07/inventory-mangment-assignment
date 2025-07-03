1ca50c65901fe00b04b48bd8bcd28c7acb48f14b
ranjith <ranjithp27122003@gmail.com>
2025-07-03 19:47:43 +0530

    fix:change in the deployment command of start

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import esbuild from 'esbuild';
+
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
   plugins: [
@@ -11,8 +16,8 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
       },
-      serverBuildFile: 'remix.js',
     }),
     tsconfigPaths(),
   ],
