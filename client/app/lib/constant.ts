dd5b3b394166f4f17fd5c31b271daadeacf91622
ranjith <ranjithp27122003@gmail.com>
2025-07-03 20:21:58 +0530

    fix:added prod url

--- client/app/lib/constant.ts
+++ client/app/lib/constant.ts
@@ -1,4 +1,4 @@
 export const BACKEND_API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://inventory-mangment-assignment-mx8l.vercel.app"
+    ? "https://inventory-mangment-assignment-2.onrender.com"
     : "http://localhost:3000";
